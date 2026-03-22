45424dcfbf1a69e8f48c479096a0a47dd9191cf4
metacubed <7028125+metacubed@users.noreply.github.com>
2026-03-23 01:58:43 +0530

    Implement config UI

--- Patcher/UserConfig.cs
+++ Patcher/UserConfig.cs
@@ -1,5 +1,18 @@
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
 internal class UserConfig
 {
-    public string Property1 = "Value 1";
-    public int    Property2 = 2;
+    [MaintainOrder]
+    [Tooltip("Clear the list to patch all mods.")]
+    public List<ModKey> ModsToPatch { get; set; } = [];
+
+    [MaintainOrder]
+    [Tooltip("If you find an unwanted staff in your patch, add it here to ignore it.")]
+    public List<IFormLinkGetter<IWeaponGetter>> ExcludedStaves { get; set; } = [];
+
+    [MaintainOrder]
+    [Tooltip("If you find an unwanted scroll in your patch, add it here to ignore it.")]
+    public List<IFormLinkGetter<IScrollGetter>> ExcludedScrolls { get; set; } = [];
 }
